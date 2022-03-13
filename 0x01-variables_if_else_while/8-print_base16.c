e56491f53ed38de56d6b99193c069e133e969fb4
Ekwee <ekweeryan@gmail,com>
2022-03-13 10:41:25 -0700

    Latest changes

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+include <stdio.h>
+/**
+ * main - prints no.s of base 16 in small letters
+ * Return: 0
+ * next line
+ */
+int main(void)
+{
+	int n;
+	char ch;
+	
+	for(n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	fot( ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar('\n');
+}
+putchar('\n');
+return (0);
+}
