285b2f1c8f935974c0ea79242f18e066140d7b22
Ekwee <ekweeryan@gmail,com>
2022-03-16 15:06:33 -0700

    rename file 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks alphabetic character
+ *
+ * Return : 1 if it is true
+ */
+
+int _isalpha(int s)
+{
+	if (s >= 'a' && s <= 'z')
+	{
+		return (1);
+	}
+	else if (s >= 'A' && s <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+	}
+}
