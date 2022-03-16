d65fb1c9e563c075c7ba5ca627c62ea88b36307e
Ekwee <ekweeryan@gmail,com>
2022-03-16 13:45:02 -0700

    reduce array lenght

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,14 +2,14 @@
 
 /**
  * main - the begining of our script to print _putchar
- * char s[9]represent our string array
+ * char s[8]represent our string array
  *return (0) 
  */
 int main(void)
 {
-	char s[9] ="_putchar";
+	char s[8] ="_putchar";
 	int i;
-     	for(i = 0 ; i <9; i++)
+     	for(i = 0 ; i <8; i++)
 	{
          _putchar(s[i]);
         }	   
