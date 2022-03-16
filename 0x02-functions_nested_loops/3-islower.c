a363e44cc0ab5208abb47f508c8fa9fd802e72b8
Ekwee <ekweeryan@gmail,com>
2022-03-16 14:48:12 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - func to tell a values case
+ *
+ * Return: 1 for sucess and 0 for failure
+ */
+
+int _islower(int s)
+{
+	if (s >= 'a' && s <= 'z')
+	{
+		return(1)
+	}
+	else
+	{
+		return(0)
+	}
+}
