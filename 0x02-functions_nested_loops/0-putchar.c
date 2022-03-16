13bd85b02006d7e66e36d02130087f86b3458c79
Ekwee <ekweeryan@gmail,com>
2022-03-16 13:18:45 -0700

    First commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - the begining of our script to print _putchar
+ * char s[9]represent our string array
+ *return (0) 
+ */
+
+int main(void){
+	char s[9] ="_putchar";
+
+for(int i = 0 ; i <9; i++){
+   _putchar(s[i]);
+}	   
+ _putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Here we practice functions and nested functions.
