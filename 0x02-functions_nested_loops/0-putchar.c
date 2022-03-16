e79e0cf5aac6c69496b7ed21e59f9423976abc8d
Ekwee <49526240+Ekwee@users.noreply.github.com>
2022-03-17 00:08:22 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,16 +3,16 @@
 /**
  * main - the begining of our script to print _putchar
  * char s[8]represent our string array
- *return (0) 
+ *return (0)
  */
 int main(void)
 {
-	char s[8] ="_putchar";
+	char s[8] = "_putchar";
 	int i;
-     	for(i = 0 ; i <8; i++)
+     	for (i = 0 ; i < 8; i++)
 	{
          _putchar(s[i]);
-        }	   
-        _putchar('\n');
-	return (0);
+	}	   
+_putchar('\n');
+return (0);
 }
